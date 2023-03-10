7dfcaaa890627f8b00cd2f0f9228efc072b5b7b8
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-03-10 09:08:32 -0500

    implement basic health_check endpoint and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("http://127.0.0.1:8007").expect("Could not bind to address");
+    run(listener)?.await
 }
