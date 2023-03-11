17cb1d9f5029331e81dd39cadd11e1bfd072c49d
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-03-11 10:39:35 -0500

    init database and restructure project

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::{configuration, routes, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("http://127.0.0.1:8007").expect("Could not bind to address");
-    run(listener)?.await
+    startup::run(listener)?.await
 }
