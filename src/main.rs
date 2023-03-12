4c21d2246965d5c5e2764f3859a3cc13cf6f929f
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-03-12 17:11:28 -0400

    implement logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 use std::net::TcpListener;
+use tracing::{subscriber::set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup};
 
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(
+        name,
+        // output to std out
+        std::io::stdout,
+    );
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Could not read configuration file");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,21 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    tracing::info!(
+        "request_id: {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+    let query_span = tracing::info_span!("Saving subscriber to database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,11 +40,19 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection_pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id: {} - Subscriber successfully saved", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(err) => {
-            println!("Failed to execute insert: {err}");
+            tracing::error!(
+                "request_id: {} - Failed to execute insert: {:?}",
+                request_id,
+                err
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
