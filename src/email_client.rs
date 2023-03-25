777d6408e43fca75ce7e513d330e03d9d884c5f1
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-03-25 13:34:39 -0400

    add test for timeout

--- src/email_client.rs
+++ src/email_client.rs
@@ -12,8 +12,12 @@ pub struct EmailClient {
 
 impl EmailClient {
     pub fn new(base_url: String, sender: SubscriberEmail, token: Secret<String>) -> Self {
+        let http_client = Client::builder()
+            .timeout(std::time::Duration::from_secs(10))
+            .build()
+            .unwrap();
         Self {
-            http_client: Client::new(),
+            http_client,
             base_url: reqwest::Url::parse(base_url.as_str()).expect("Could not parse URL"),
             sender,
             token
@@ -164,6 +168,32 @@ mod tests {
             .await;
         // Assert
         assert_err!(outcome);
+    }
 
+    #[tokio::test]
+    async fn send_email_times_out_if_server_takes_too_long() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(
+            mock_server.uri(),
+            sender,
+            Secret::new(Faker.fake())
+        );
+        let response = ResponseTemplate::new(200)
+            .set_delay(std::time::Duration::from_secs(180));
+        Mock::given(any())
+            .respond_with(response)
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let body: String = Paragraph(1..10).fake();
+        // Act
+        let outcome = email_client.send_email(subscriber_email, &subject, &body, &body)
+            .await;
+        // Assert
+        assert_err!(outcome);
     }
 }
