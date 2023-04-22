6fd67e59a23bf56b9b9a82b35a2390f61f1b6684
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-04-22 10:37:48 -0400

    re-add deleted login html

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -10,5 +10,33 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(error_html)
+        .body(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {error_html}
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+            >
+        </label>
+        <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            >
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+        ))
 }
