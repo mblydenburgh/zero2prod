2e37bcb5179abc29168cd9e14e3e1675e6a3178e
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-04-23 09:12:35 -0400

    use actix flash message middleware to remove dealing with cookie api directly, force cookies to be signed by hmac key

--- src/startup.rs
+++ src/startup.rs
@@ -3,10 +3,13 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
-use secrecy::Secret;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -35,7 +38,13 @@ impl Application {
         let address = format!("{}:{}", config.application.host, config.application.port);
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client, base_url)?;
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            base_url,
+            config.application.hmac_secret,
+        )?;
         Ok(Self { port, server })
     }
     pub fn port(&self) -> u16 {
@@ -54,21 +63,23 @@ pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
 
 pub struct ApplicationBaseUrl(pub String);
 
-#[derive(Clone)]
-pub struct HmacSecret(pub Secret<String>);
-
 pub fn run(
     listener: TcpListener,
     connection_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let connection_pool = Data::new(connection_pool);
     let email_client = Data::new(email_client);
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .wrap(message_framework.clone())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
             .route("/login", web::get().to(login_form))
