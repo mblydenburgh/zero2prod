73d9e74f340666f22f136a3a9d00dbd662eb2c8e
Blydenburgh, Michael <Michael.Blydenburgh@LibertyMutual.com>
2023-04-23 11:06:35 -0400

    add basic redis config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> anyhow::Result<()> {
     // subscriber helps nicely format all logs from application and middleware, and output to std out
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,8 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_session::storage::RedisSessionStore;
+use actix_session::SessionMiddleware;
 use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::web::Data;
@@ -21,7 +23,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(config: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(config: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&config.database);
         let base_url = config.application.base_url;
         let sender = config
@@ -44,7 +46,9 @@ impl Application {
             email_client,
             base_url,
             config.application.hmac_secret,
-        )?;
+            config.redis_uri,
+        )
+        .await?;
         Ok(Self { port, server })
     }
     pub fn port(&self) -> u16 {
@@ -63,23 +67,29 @@ pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
 
 pub struct ApplicationBaseUrl(pub String);
 
-pub fn run(
+pub async fn run(
     listener: TcpListener,
     connection_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
     let connection_pool = Data::new(connection_pool);
     let email_client = Data::new(email_client);
-    let message_store =
-        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let redis_store = RedisSessionStore::new(redis_uri.expose_secret()).await?;
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
     let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .wrap(message_framework.clone())
+            .wrap(SessionMiddleware::new(
+                redis_store.clone(),
+                secret_key.clone(),
+            ))
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
             .route("/login", web::get().to(login_form))
